0cf910201e7aea17a2b59872530f7d7d30612c96
Thais Giovanna <thaisgiovanna.tg@gmail.com>
2025-10-20 23:17:46 -0300

    Adicionado novos exercicios

---
+++ media_aritimetica.py
@@ -0,0 +1,6 @@
+"""Desenvolva um programa que leia as duas notas de um aluno e calcule e mostre sua média:"""
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite sua segunda nota: '))
+media = (nota1 + nota2) / 2
+print('Sua média é: ', media)
